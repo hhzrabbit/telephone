498bb169be9918ab549db8176b2c536db82a2adc
Haley Zeng <hzeng@stuy.edu>
2016-12-11 12:42:53 -0500

    broke up interact into helpers and finished the fgets storing

--- interact.c
+++ interact.c
@@ -8,37 +8,57 @@
 #include <sys/shm.h>
 #include <fcntl.h>
 
+void printLastLine(int * shm){
+  int fd;
+  int amtToRead = * shm;
+  char buf[amtToRead + 1];
+  
+  fd = open("story.txt", O_RDONLY, 0644);
+  lseek(fd, (-1 * amtToRead), SEEK_END);
+  
+  read(fd, &buf, amtToRead);
+  buf[amtToRead] = 0;
+
+  printf("Last line of story:\n");
+  printf("%s\n", buf);
+  close(fd);
+
+}
+
+void getAndSaveNextLine(int * shm){
+  char d[1024];
+  char * dest = d;
+  int fd;
+  int lenInput;
+
+  printf("Enter next line: ");
+  fgets(dest, 1024, stdin);
+  
+  lenInput = strlen(d);
+  * shm = lenInput;
+  
+  fd = open("story.txt", O_WRONLY | O_APPEND, 0644);
+  
+  write(fd, d, lenInput);
+  close(fd);
+
+  printf("Line added: %s\n", d);
+
+}
+
 int main(){
   int key = ftok("README.md", 22);
   int semid;
   int shmid;
   int * shm;
-  int amtToRead;
   int sc;
-  int fd;
 
   shmid = shmget(key, 4, 0644);
   shm = shmat(shmid, 0, 0);
-  amtToRead = * shm;
 
-  fd = open("story.txt", O_RDONLY, 0644);
+  printLastLine(shm);
 
-  char buf[amtToRead + 1];
-  
-  read(fd, &buf, amtToRead);
-  buf[amtToRead] = 0;
-  
-  printf("%s\n", buf);
-  close(fd);
+  getAndSaveNextLine(shm);
 
-  printf("\n\nEnter next line: ");
-  
-  char d[1024];
-  char * dest = d;
-  fgets(dest, 256, stdin);
-
-  
-  printf("len: %ld\n", strlen(d));
-  
   return 0;
 }
